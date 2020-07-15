adc8de3da60f5b1c68b7a50507742f7fe9d916e4
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-07-15 11:18:28 +0530

    Update docker.sh

--- tools/travis/docker.sh
+++ tools/travis/docker.sh
@@ -35,10 +35,13 @@ sudo apt-key fingerprint 0EBFCD88
 # This is required because libseccomp2 (>= 2.3.0) is not provided in trusty by default
 sudo add-apt-repository -y ppa:ubuntu-sdk-team/ppa
 
-#sudo add-apt-repository \
-#    "deb [arch=$(uname -m | sed -e 's/x86_64/amd64/g')] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+if [ "${TRAVIS_CPU_ARCH}" == "arm64" ]; then
 sudo add-apt-repository \
-    "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    "deb [arch=$(uname -m | sed -e 's/x86_64/amd64/g')] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
+else
+sudo add-apt-repository \
+    "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
+fi
 
 sudo apt-get update
 sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -y install docker-ce=18.06.3~ce~3-0~ubuntu containerd.io
